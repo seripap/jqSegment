45c4973b3fb1eb48743d7e0196332c1872d9dbe1
Daniel Seripap <d@seripap.com>
2013-01-10 08:54:24 -0800

    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-jqSegment
-=========
-
 jq.Segment - lightweight jQuery Plugin that grabs URL Segments
+=========
+##v0.1
+##By Daniel Seripap, daniel@seripap.com
+=========
+Grabs URL segments of website.
+
+###USAGE:
+**Example URL:** http://www.seripap.com/a/b/c
+
+$.segment() - returns _a,b,c_  
+$.segment(1) - returns _a_  
+$.segment(2) - returns _b_  
+$.segment(3) - returns _c_
---
+++ jq.segment-v0.1.js
@@ -0,0 +1,36 @@
+/*
+	jq.Segment - lightweight jQuery Plugin that grabs URL Segments
+	v0.1
+	By Daniel Seripap, daniel@seripap.com
+	----------------------------------------
+	Grabs URL segments of website.
+
+	USAGE:
+	Example URL: http://www.seripap.com/a/b/c
+
+	$.segment() - returns a,b,c
+	$.segment(1) - returns a
+	$.segment(2) - returns b
+	$.segment(3) - returns c
+*/
+
+	(function ($) { "use strict";
+		Array.prototype.clean = function(x) {
+			for (var i = 0; i < this.length; i++) {
+				if (this[i] === x) {
+					this.splice(i, 1);
+					i--;
+				}
+			}
+			return this;
+		};
+		$.segment = function(segment) {
+			var url = window.location.pathname,
+			segments = url.split('/'[0]).clean('');
+
+			if (segment) {
+				return segments[segment-1];
+			} else {
+				return segments;
+			}
+		};})(jQuery);
